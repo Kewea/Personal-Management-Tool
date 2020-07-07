5fd0af3b367ab4df8dc4c65a4f95427328c50f60
kwanwai.wong <kwanwai.wong@rakuten.com>
2020-07-07 15:44:51 +0900

    Introduce redux

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,30 +1,25 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from "react-redux";
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './component/Dashboard';
 import Header from './component/Layout/Header';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import AddProject from './component/Project/AddProject';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-    <Header />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <Dashboard />
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
